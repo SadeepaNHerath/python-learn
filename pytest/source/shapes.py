e175b09ef6cb0bdf02ad7712c0383b1cd2de77b6
SadeepaNHerath <sadeepahearth@gmail.com>
2025-01-06 07:34:00 +0530

    class based testing

---
+++ pytest/source/shapes.py
@@ -0,0 +1,38 @@
+import math
+
+class Shape:
+
+    def area(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def perimeter(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius * self.radius
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+class Rectangle(Shape):
+
+        def __init__(self, length, width):
+            self.length = length
+            self.width = width
+
+        def __eq__(self, other):
+            if not isinstance(other, Rectangle):
+                return False
+            return self.length == other.length and self.width == other.width
+
+        def area(self):
+            return self.length * self.width
+
+        def perimeter(self):
+            return 2 * (self.length + self.width)
