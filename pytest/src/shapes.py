3850c17043101cb5e0bd8d331015ee7eaa253fdc
SadeepaNHerath <sadeepahearth@gmail.com>
2025-01-06 15:46:25 +0530

    parameterized

--- pytest/src/shapes.py
+++ pytest/src/shapes.py
@@ -22,17 +22,22 @@ class Circle(Shape):
 
 class Rectangle(Shape):
 
-        def __init__(self, length, width):
-            self.length = length
-            self.width = width
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
 
-        def __eq__(self, other):
-            if not isinstance(other, Rectangle):
-                return False
-            return self.length == other.length and self.width == other.width
+    def __eq__(self, other):
+        if not isinstance(other, Rectangle):
+            return False
+        return self.length == other.length and self.width == other.width
 
-        def area(self):
-            return self.length * self.width
+    def area(self):
+        return self.length * self.width
 
-        def perimeter(self):
-            return 2 * (self.length + self.width)
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+class Square(Rectangle):
+
+    def __init__(self, side_length):
+        super().__init__(side_length, side_length)
